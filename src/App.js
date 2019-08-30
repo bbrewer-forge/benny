4493a4e3883f4b6942369859011212337eb274b9
cameronmarsh <cmarsh@pillartechnology.com>
2019-08-30 11:05:41 -0400

    example of using jarred.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+// import { TodaysDate } from 'shared-react-components'
+import { ErrorComponent } from 'discovery'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        {/* <TodaysDate /> */}
+        <ErrorComponent errorText="stuff" />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
